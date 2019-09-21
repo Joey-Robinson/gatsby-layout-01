cd9048c8b7b742f7c9011280d5d45e8729688995
Joey Robinson <theamazingness@gmail.com>
2019-09-20 22:20:38 -0400

    Adjusted contact.js to have a small paragraph greeting. Removed comment(s) from _contact.scss.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,17 @@ const Contact = () => (
   <Layout>
     <section className="contact">
       <Header />
-      <h1 className="animated--heading">Contact Me</h1>
+      <h1 className="animated--heading">Find Me On The Web</h1>
+      <div className="contact--greeting">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vel quidem
+          fugiat voluptas corrupti quam inventore numquam unde. Veritatis
+          architecto officia fuga hic accusantium, nulla dolor inventore
+          assumenda dolorum vel saepe alias aspernatur iusto corrupti, deserunt
+          magnam, amet expedita ad temporibus molestias. Sunt, itaque
+          reprehenderit.
+        </p>
+      </div>
       <ContactList />
       <ResumeDownload />
     </section>
